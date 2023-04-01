3b9359319f8140d65152ee0510f38994d03326a7
okagawa <109455+okagawa@users.noreply.github.com>
2023-04-01 20:34:31 +0900

    python code updated w/ cache mechanism

--- count_sum_of_numerator_1_fractions.py
+++ count_sum_of_numerator_1_fractions.py
@@ -10,38 +10,70 @@ def euclidean(m, n):
 
 
 # q/pをm個の分数で合計する組み合わせの数を数える関数
-def count_fractions(q, p, m, prev_r):
-    count = 0
-    if m == 1:
-        if q == 1:
-            return 1
+def count_fractions(q, p, n, dic):
+    def count_fractions_sub(q, p, m, prev_r, hist, dic):
+        count = 0
+        if m == 1:
+#            if q == 1:
+#                hist.append(r)
+#                # print(hist.append(r))
+#                hist=hist[:-1]
+#                return 1, hist, dic
+#            else:
+#                return 0, hist, dic
+            pass
         else:
-            return 0
-    else:
-        # q/pを超えない1/rを満足するrの最小値を求める
-        r_min = max(math.ceil(p/q), prev_r)
-        r_max = math.floor(p*m/q)
+            # q/pを超えない1/rを満足するrの最小値を求める
+            r_min = max(math.ceil(p/q), prev_r)
+            r_max = math.floor(p*m/q)
 
-        # q/pから1/rを引いた残りが1/r*(m-1)より大きいと、rが大きすぎるのでNG
-        # 1/r*(m-1) >= q/p - 1/r
-        r = r_max
-        while r >= r_min:
-            next_q, next_p = q*r-p, p*r
-            if next_q > 0:
-                if m == 2:
-                    if next_p % next_q == 0:
-                        count = count + 1
+            # m=2(残り2個)かつ、q/p=1/r_minの場合は特別ケース。
+            # 1/r_min - 1/r の分母が分子で割り切れるかどうかで判別すればよい。
+            if  m == 2 and q == 1 and p == r_min:
+                if p==r_min and p in dic:
+                    count = count + dic[p]
+                    # print(f'q/p={q}/{p}, dic[{p}]={dic[p]}')
                 else:
-                    e = euclidean(next_q,next_p)
-                    next_q, next_p = int(next_q/e), int(next_p/e)
-                    c = count_fractions(next_q, next_p, m-1, r)
+                    c = 0
+                    for r in range(r_min+1, r_max+1):
+                        if (r_min * r) % (r - r_min) == 0:
+                            c = c +1
+                    dic[p] = c
+                    # print(f'dic[{p}]={c}')
                     count = count + c
-            r = r - 1
+            else:
+                # q/pから1/rを引いた残りが1/r*(m-1)より大きいと、rが大きすぎるのでNG
+                # 1/r*(m-1) >= q/p - 1/r
+                for r in range(r_min, r_max+1):
+                    next_q, next_p = q*r-p, p*r
+                    if next_q > 0:
+                        if m == 2: # q != 1
+                            c = 0
+                            if next_p % next_q == 0:
+                                hist.append(r)
+                                hist.append(int(next_p/next_q))
+                                # print(hist)
+                                hist = hist[:-2]
+                                c = c + 1
+                            count = count + c
+                        else:
+                            e = euclidean(next_q,next_p)
+                            next_q, next_p = int(next_q/e), int(next_p/e)
+                            hist.append(r)
+                            c,hist,dic = count_fractions_sub(next_q, next_p, m-1, r, hist, dic)
+                            hist = hist[:-1]
+                            count = count + c
 
-        return count
+            return count,hist, dic
+    count, _, dic = count_fractions_sub(q, p, n, 1, [], dic)
+    return count, dic
 
 if __name__=='__main__':
-    n = 7
-    c = count_fractions(1,1,n,1)
-    print(c)
+    n_max = 7
+    dic = dict()
+    for n in range(2, n_max+1):
+        c, dic = count_fractions(1, 1, n, dic)
+        print(f'n={n}, c={c}')
+        # print(dic)
+        print()
 
